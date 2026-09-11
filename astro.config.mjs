3563ae7eccfc4e44f40bca53d804717fb27c775c
Bruno Martinez <s89905765@gmail.com>
2026-09-10 20:51:59 -0600

    feat: actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  server: {
-    host: true, // Necesario para que funcione la IP 192.168.100.6
-    port: 4322, 
-  },
-  vite: {
-    server: {
-      hmr: {
-        host: '192.168.100.6', // IP local para que el WebSocket conecte
-      },
-      cors: true,
-    },
-    css: { devSourcemap: true },
-  },
+  server: { host: true, port: 4322 },
+  devToolbar: { enabled: false },
+  vite: { css: { devSourcemap: true } },
   markdown: { syntaxHighlight: 'prism' },
 });
