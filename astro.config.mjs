7d74b1cb8a7f3928a3d046f5146dc8043d60e3d9
Bruno Martinez <s89905765@gmail.com>
2026-09-10 19:50:49 -0600

    feat: actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,45 +1,19 @@
 // astro.config.mjs
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   server: {
     host: true, // Necesario para que funcione la IP 192.168.100.6
-    port: 4321,
+    port: 4321, 
   },
   vite: {
     server: {
-      // allowedHosts: ['tapioca-applicant-deem.ngrok-free.dev'],
-      // ws: { clientPort: 443 },
       hmr: {
-        host: "192.168.100.6", // Tu IP local actual
+        host: '192.168.100.6', // IP local para que el WebSocket conecte
       },
       cors: true,
     },
     css: { devSourcemap: true },
   },
-  markdown: { syntaxHighlight: "prism" },
+  markdown: { syntaxHighlight: 'prism' },
 });
-/* security: {
-    csp: {
-      scriptDirective: {
-        resources: [
-          "'self'", 
-          "'unsafe-inline'", 
-          "'unsafe-eval'", 
-          "https://kit.fontawesome.com"
-        ]
-      },
-      styleDirective: {
-        resources: [
-          "'self'", 
-          "'unsafe-inline'"
-        ]
-      },
-      directives: [
-        "font-src 'self' https://kit.fontawesome.com https://fonts.gstatic.com data:",
-        "img-src 'self' data: https:",
-        // ✅ Permitimos explícitamente WebSockets en cualquier puerto local
-        "connect-src 'self' https://kit.fontawesome.com ws://localhost:* wss://localhost:*",
-      ],
-    },
-  },*/
